2eb22c6992a7976ee53e4d9c4cfc512955fec2e7
Jonline CI <ci@jonline.io>
2026-07-20 01:31:14 +0000

    jonline 0.5.540-475850d

--- Formula/jonline.rb
+++ Formula/jonline.rb
@@ -4,9 +4,9 @@
 class Jonline < Formula
   desc "Jonline federated social server"
   homepage "https://github.com/jonlatane/jonline"
-  url "https://github.com/jonlatane/jonline/releases/download/vPLACEHOLDER/jonline-PLACEHOLDER-macos-arm64.tar.gz"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
-  version "PLACEHOLDER"
+  url "https://github.com/jonlatane/jonline/releases/download/v0.5.540-475850d/jonline-0.5.540-475850d-macos-arm64.tar.gz"
+  sha256 "f5df8d652fa460acab6dce32f7a386d5aca7c4b879e94ec7e6a78d6b6c628a6b"
+  version "0.5.540-475850d"
   license "AGPL-3.0-only"
 
   depends_on arch: :arm64
@@ -14,10 +14,10 @@ class Jonline < Formula
 
   def install
     (etc/"jonline").install Dir["*"]
-    (bin/"jonline").write <<~EOS
+    (bin/"jonline").write <<~SCRIPT
       #!/bin/bash
       cd "#{etc}/jonline" && exec ./jonline "$@"
-    EOS
+    SCRIPT
     chmod 0755, bin/"jonline"
   end
 
