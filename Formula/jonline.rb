aa14d4d1ab86b7505deeb9f76650ffd3e8f29ca4
Jonline CI <ci@jonline.io>
2026-08-20 20:54:57 +0000

    jonline 0.5.552-80c15fd

--- Formula/jonline.rb
+++ Formula/jonline.rb
@@ -5,9 +5,9 @@
 class Jonline < Formula
   desc "Jonline federated social server"
   homepage "https://github.com/jonlatane/jonline"
-  url "https://github.com/jonlatane/jonline/releases/download/v0.5.552-1c42158/jonline-0.5.552-1c42158-macos-arm64.tar.gz"
-  sha256 "9a8aa297931de12dd03e85d825b7957ca830e9dd821c07178ef690be0df37b90"
-  version "0.5.552-1c42158"
+  url "https://github.com/jonlatane/jonline/releases/download/v0.5.552-80c15fd/jonline-0.5.552-80c15fd-macos-arm64.tar.gz"
+  sha256 "a7def9b6005e67a16ddc160e445d93b1f43c9517f1265054cef0bcff4bfae9d3"
+  version "0.5.552-80c15fd"
   license "AGPL-3.0-only"
 
   depends_on arch: :arm64
